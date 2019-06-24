7edad083d99c13b1749e7b070fe2857b103dce37
Erik <erik.hac@outlook.com>
2019-06-23 22:51:29 -0300

    Adiciona indicador de progresso para o request na gipfy API

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -54,9 +54,36 @@ class _HomePageState extends State<HomePage> {
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
             ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError) {
+                      return Container();
+                    } else
+                      _createGifTable(context, snapshot) {}
+                }
+              },
+            ),
           )
         ],
       ),
     );
   }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {}
 }
